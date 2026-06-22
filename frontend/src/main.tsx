1ddf89ca4d66b939f1446ab0f5884d8c8eb84cc7
Ahmad Rauf <ahmadrauf036@gmail.com>
2026-06-22 16:25:35 +0500

    Removed some console.log from code!

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./routes/routes"; 
-import { supabase } from "./lib/supabase.utils"; 
-import { useAuthStore } from "./modules/auth/auth.store"; 
+import { router } from "./routes/routes";
+import { supabase } from "./lib/supabase.utils";
+import { useAuthStore } from "./modules/auth/auth.store";
 import "./index.css";
 import { Toaster } from "sonner";
 function Root() {
@@ -12,7 +12,8 @@ function Root() {
     useEffect(() => {
         // Check if a session already exists on app load
         supabase.auth.getSession().then(({ data: { session } }) => {
-            console.log("Session on load:", session); // remove later
+            if (process.env.ENV === "DEVELOPMENT")
+                console.log("Session on load:", session); // remove later
             setSession(session);
         });
 
@@ -20,7 +21,8 @@ function Root() {
         const {
             data: { subscription },
         } = supabase.auth.onAuthStateChange((_event, session) => {
-            console.log("Auth state changed:", _event, session); // remove later
+            if (process.env.ENV == "DEVELOPMENT")
+                console.log("Auth state changed:", _event, session); // remove later
             setSession(session);
         });
 
